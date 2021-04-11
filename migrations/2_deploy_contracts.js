8fdbcf0daef4aec6bd753221a5e9326a22416044
Mliviusz <dliviusz1@gmail.com>
2021-04-11 19:15:34 +0200

    KYC contract, truffle_cli_local network

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,13 @@
 var MyToken = artifacts.require("MyToken.sol");
-var MyTokenSale = artifacts.require("MyTokenSale.sol");
+var MyTokenSale = artifacts.require("MyTokenSale");
+var KycContract = artifacts.require("KycContract");
+require("dotenv").config({path: "../.env"});
 
 module.exports = async function(deployer) { 
     let addr = await web3.eth.getAccounts();
-    await deployer.deploy(MyToken, 1000000);
-    await deployer.deploy(MyTokenSale, 1, addr[0], MyToken.address);
+    await deployer.deploy(MyToken, process.env.INITIAL_TOKENS);
+    await deployer.deploy(KycContract);
+    await deployer.deploy(MyTokenSale, 1, addr[0], MyToken.address, KycContract.address);
     let instance = await MyToken.deployed();
-    await instance.transfer(MyTokenSale.address, 1000000);
+    await instance.transfer(MyTokenSale.address, process.env.INITIAL_TOKENS);
 };  
